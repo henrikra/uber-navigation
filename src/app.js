4301395f4e8c9e8935da98b9e9ccae63a69b0df0
Henrik Raitasola <henrik.raitasola@wolt.com>
2017-03-31 15:41:20 +0300

    Put common stuff to app.js

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import { Navigation } from 'react-native-navigation';
+
+import { registerScreens } from '../screens';
+registerScreens();
+
+Navigation.startSingleScreenApp({
+  screen: {
+    screen: 'uber.MapScreen',
+    navigatorStyle: {
+      navBarHidden: true,
+    }
+  },
+  drawer: {
+    left: {
+      screen: 'uber.menu',
+    },
+    disableOpenGesture: false,
+  },
+});
